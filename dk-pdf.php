8e03137f4403e7933457576c93721fc12850e221
Dinamiko <emili.castells@dinamiko.com>
2017-09-02 21:23:41 +0200

    Refactor assets.

--- dk-pdf.php
+++ dk-pdf.php
@@ -38,6 +38,12 @@ function initialize() {
 			require_once __DIR__ . '/vendor/autoload.php';
 		}
 
+		load_plugin_textdomain(
+			'dkpdf',
+			false,
+			dirname( plugin_basename( __FILE__ ) ) . '/languages'
+		);
+
 		( new DKPDF )->init();
 
 	} catch ( \Throwable $e ) {
@@ -50,91 +56,3 @@ function initialize() {
 }
 
 add_action( 'plugins_loaded', __NAMESPACE__ . '\\initialize' );
-
-/*
-if ( ! class_exists( 'DKPDF' ) ) {
-
-	final class DKPDF {
-
-		private static $instance;
-
-		public static function instance() {
-
-			if ( ! isset( self::$instance ) && ! ( self::$instance instanceof DKPDF ) ) {
-
-				self::$instance = new DKPDF;
-
-				self::$instance->setup_constants();
-
-				add_action( 'plugins_loaded', array( self::$instance, 'dkpdf_load_textdomain' ) );
-
-				self::$instance->includes();
-
-			}
-
-			return self::$instance;
-
-		}
-
-		public function dkpdf_load_textdomain() {
-
-			load_plugin_textdomain( 'dkpdf', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-
-		}
-
-		private function setup_constants() {
-
-			if ( ! defined( 'DKPDF_VERSION' ) ) { define( 'DKPDF_VERSION', '1.9.2' ); }
-			if ( ! defined( 'DKPDF_PLUGIN_DIR' ) ) { define( 'DKPDF_PLUGIN_DIR', plugin_dir_path( __FILE__ ) ); }
-			if ( ! defined( 'DKPDF_PLUGIN_URL' ) ) { define( 'DKPDFPLUGIN_URL', plugin_dir_url( __FILE__ ) ); }
-			if ( ! defined( 'DKPDF_PLUGIN_FILE' ) ) { define( 'DKPDF_PLUGIN_FILE', __FILE__ ); }
-
-		}
-
-		private function includes() {
-
-			// settings / metaboxes
-			if ( is_admin() ) {
-
-				require_once DKPDF_PLUGIN_DIR . 'includes/class-dkpdf-settings.php';
-				$settings = new DKPDF_Settings( $this );
-
-				require_once DKPDF_PLUGIN_DIR . 'includes/class-dkpdf-admin-api.php';
-				$this->admin = new DKPDF_Admin_API();
-
-				require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-metaboxes.php';
-
-			}
-
-			// load css / js
-			require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-load-js-css.php';
-
-			// functions
-			require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-functions.php';
-
-			// shortcodes
-			require_once DKPDF_PLUGIN_DIR . 'includes/class-dkpdf-template-loader.php';
-			require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-shortcodes.php';
-
-		}
-
-		public function __clone() {
-			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'dkpdf' ), DKPDF_VERSION );
-		}
-
-		public function __wakeup() {
-			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'dkpdf' ), DKPDF_VERSION );
-		}
-
-	}
-
-}
-
-function DKPDF() {
-
-	return DKPDF::instance();
-
-}
-
-DKPDF();
-*/
