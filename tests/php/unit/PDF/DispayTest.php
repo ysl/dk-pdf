30683d8238aef2c405640230743a3fe4213ddb72
Emilio Castells <emili.castells@dinamiko.com>
2018-01-05 22:42:31 +0100

    Fix unit test global issue.

--- tests/php/unit/PDF/DispayTest.php
+++ tests/php/unit/PDF/DispayTest.php
@@ -36,10 +36,39 @@ class Display_Test extends \PHPUnit\Framework\TestCase {
 		\Brain\Monkey\Functions\when( 'get_query_var' )
 			->justReturn( null );
 
+/*
+\Brain\Monkey\Functions\when('get_post')
+->justReturn((object)array(
+'ID' => 12
+));
+*/
+
+		global $post;
+		$post = new class {
+			public $ID;
+		};
+		$post->ID = 1;
+
+		\Brain\Monkey\Functions\when( 'get_post_type' )
+			->justReturn( 'post' );
+
+		\Brain\Monkey\Functions\when( 'get_option' )
+			->justReturn( array('post') );
+
+		\Brain\Monkey\Functions\when( 'is_archive' )
+			->justReturn( false );
+		\Brain\Monkey\Functions\when( 'is_front_page' )
+			->justReturn( false );
+		\Brain\Monkey\Functions\when( 'is_home' )
+			->justReturn( false );
+
+
+		/*
 		// TODO Fix test.
 		$this->markTestIncomplete(
 			'Error: Call to undefined function Dinamiko\DKPDF\PDF\get_post_type()'
 		);
+		*/
 
 		$template = $this->createMock( TemplateLoader::class );
 		$display = new Display( $template );
