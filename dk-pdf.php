de3cdd958bf230ef47b358daca636bb2419aaf75
dinamiko <e.castells@inpsyde.com>
2018-09-22 14:17:57 +0200

    Automatic code format
    #66

--- dk-pdf.php
+++ dk-pdf.php
@@ -13,115 +13,124 @@
  * Domain Path: /languages/
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
-if ( ! class_exists( 'DKPDF' ) ) {
+if (!class_exists('DKPDF')) {
 
-	final class DKPDF {
+    final class DKPDF
+    {
 
-		private static $instance;
+        private static $instance;
 
-		public static function instance() {
+        public static function instance()
+        {
 
-			if ( ! isset( self::$instance ) && ! ( self::$instance instanceof DKPDF ) ) {
+            if (!isset(self::$instance) && !(self::$instance instanceof DKPDF)) {
 
-				self::$instance = new DKPDF;
+                self::$instance = new DKPDF;
 
-				self::$instance->setup_constants();
+                self::$instance->setup_constants();
 
-				add_action( 'plugins_loaded', array( self::$instance, 'dkpdf_load_textdomain' ) );
+                add_action('plugins_loaded', array(self::$instance, 'dkpdf_load_textdomain'));
 
-				self::$instance->includes();
+                self::$instance->includes();
 
-			}
+            }
 
-			return self::$instance;
+            return self::$instance;
 
-		}
+        }
 
-		public function dkpdf_load_textdomain() {
+        public function dkpdf_load_textdomain()
+        {
 
-			load_plugin_textdomain( 'dkpdf', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+            load_plugin_textdomain('dkpdf', false,
+                dirname(plugin_basename(__FILE__)) . '/languages');
 
-		}
+        }
 
-		private function setup_constants() {
+        private function setup_constants()
+        {
 
-			if ( ! defined( 'DKPDF_VERSION' ) ) {
-				define( 'DKPDF_VERSION', '1.9.6' );
-			}
-			if ( ! defined( 'DKPDF_PLUGIN_DIR' ) ) {
-				define( 'DKPDF_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-			}
-			if ( ! defined( 'DKPDF_PLUGIN_URL' ) ) {
-				define( 'DKPDFPLUGIN_URL', plugin_dir_url( __FILE__ ) );
-			}
-			if ( ! defined( 'DKPDF_PLUGIN_FILE' ) ) {
-				define( 'DKPDF_PLUGIN_FILE', __FILE__ );
-			}
+            if (!defined('DKPDF_VERSION')) {
+                define('DKPDF_VERSION', '1.9.6');
+            }
+            if (!defined('DKPDF_PLUGIN_DIR')) {
+                define('DKPDF_PLUGIN_DIR', plugin_dir_path(__FILE__));
+            }
+            if (!defined('DKPDF_PLUGIN_URL')) {
+                define('DKPDFPLUGIN_URL', plugin_dir_url(__FILE__));
+            }
+            if (!defined('DKPDF_PLUGIN_FILE')) {
+                define('DKPDF_PLUGIN_FILE', __FILE__);
+            }
 
-		}
+        }
 
-		private function includes() {
+        private function includes()
+        {
 
-			// settings / metaboxes
-			if ( is_admin() ) {
+            // settings / metaboxes
+            if (is_admin()) {
 
-				require_once DKPDF_PLUGIN_DIR . 'includes/class-dkpdf-settings.php';
-				$settings = new DKPDF_Settings( $this );
+                require_once DKPDF_PLUGIN_DIR . 'includes/class-dkpdf-settings.php';
+                $settings = new DKPDF_Settings($this);
 
-				require_once DKPDF_PLUGIN_DIR . 'includes/class-dkpdf-admin-api.php';
-				$this->admin = new DKPDF_Admin_API();
+                require_once DKPDF_PLUGIN_DIR . 'includes/class-dkpdf-admin-api.php';
+                $this->admin = new DKPDF_Admin_API();
 
-				require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-metaboxes.php';
+                require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-metaboxes.php';
 
-			}
+            }
 
-			// load css / js
-			require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-load-js-css.php';
+            // load css / js
+            require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-load-js-css.php';
 
-			// functions
-			require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-functions.php';
+            // functions
+            require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-functions.php';
 
-			// shortcodes
-			require_once DKPDF_PLUGIN_DIR . 'includes/class-dkpdf-template-loader.php';
-			require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-shortcodes.php';
+            // shortcodes
+            require_once DKPDF_PLUGIN_DIR . 'includes/class-dkpdf-template-loader.php';
+            require_once DKPDF_PLUGIN_DIR . 'includes/dkpdf-shortcodes.php';
 
-		}
+        }
 
-		public function __clone() {
+        public function __clone()
+        {
 
-			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'dkpdf' ), DKPDF_VERSION );
-		}
+            _doing_it_wrong(__FUNCTION__, __('Cheatin&#8217; huh?', 'dkpdf'), DKPDF_VERSION);
+        }
 
-		public function __wakeup() {
+        public function __wakeup()
+        {
 
-			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'dkpdf' ), DKPDF_VERSION );
-		}
+            _doing_it_wrong(__FUNCTION__, __('Cheatin&#8217; huh?', 'dkpdf'), DKPDF_VERSION);
+        }
 
-	}
+    }
 
 }
 
-function DKPDF() {
+function DKPDF()
+{
 
-	if ( version_compare( phpversion(), '5.6.0', '<' ) ) {
+    if (version_compare(phpversion(), '5.6.0', '<')) {
 
-		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-		deactivate_plugins( '/dk-pdf/dk-pdf.php' );
+        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+        deactivate_plugins('/dk-pdf/dk-pdf.php');
 
-		wp_die(
-			'<p>' . 'DK PDF can not be activated because it requires at least PHP version 5.6.0. '
-			. 'In case you can not update PHP, here you can <a href="'. esc_url('https://github.com/Dinamiko/dk-pdf/releases/tag/v1.9.3') .'" target="_blank">download DK PDF 1.9.3</a> which works with PHP 5.3 and above.'
-			. '</p>'
-			. '<a href="' . admin_url( 'plugins.php' ) . '">' . esc_attr__( 'Back', 'dkpdf' ) . '</a>'
-		);
-	} else {
+        wp_die(
+            '<p>' . 'DK PDF can not be activated because it requires at least PHP version 5.6.0. '
+            . 'In case you can not update PHP, here you can <a href="' . esc_url('https://github.com/Dinamiko/dk-pdf/releases/tag/v1.9.3') . '" target="_blank">download DK PDF 1.9.3</a> which works with PHP 5.3 and above.'
+            . '</p>'
+            . '<a href="' . admin_url('plugins.php') . '">' . esc_attr__('Back', 'dkpdf') . '</a>'
+        );
+    } else {
 
-		return DKPDF::instance();
-	}
+        return DKPDF::instance();
+    }
 }
 
 DKPDF();
--- includes/dkpdf-upgrades.php
+++ includes/dkpdf-upgrades.php
@@ -1,40 +1,48 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-/**
-* adds dashboard page 
-*/
-function dkpdf_welcome_screen_page(){
-    add_dashboard_page('DK PDF Welcome', 'DK PDF Welcome', 'manage_options', 'dkpdf-welcome', 'dkpdf_welcome_page');
+if (!defined('ABSPATH')) {
+    exit;
 }
 
-// output dkpdf-welcome dashboard page 
-function dkpdf_welcome_page(){ ?>
+/**
+ * adds dashboard page
+ */
+function dkpdf_welcome_screen_page()
+{
+    add_dashboard_page('DK PDF Welcome', 'DK PDF Welcome', 'manage_options', 'dkpdf-welcome',
+        'dkpdf_welcome_page');
+}
+
+// output dkpdf-welcome dashboard page
+function dkpdf_welcome_page()
+{ ?>
 
     <div class="wrap">
 
-      <h1>Welcome to DK PDF <?php echo DKPDF_VERSION;?></h1>
-      <h2 style="font-size:140%;">What's new in this version:</h2>
-      <ul>
-        <li>
-          <h3 style="margin-top:20px;">DK PDF admin menu</h3>
-          <?php 
-            $img1 = plugins_url( 'assets/images/dkpdf-admin-menu.jpg', DKPDF_PLUGIN_FILE );
-          ?>
-          <img style="margin-bottom:20px;width:100%;height:auto;"src="<?php echo $img1;?>">
-        </li>
-        <li>
-          <h3 style="margin-top:20px;">PDF Setup tab for adjusting page orientation, font size and margins of the PDF</h3>
-          <?php 
-            $img2 = plugins_url( 'assets/images/dkpdf-setup-tab.jpg', DKPDF_PLUGIN_FILE );
-          ?>
-          <img style="margin-bottom:20px;width:100%;height:auto;"src="<?php echo $img2;?>">
-        </li>
-        <li>
-          <h3 style="margin-top:20px;">[dkpdf-remove] shortcode for removing pieces of content in the generated PDF</h3></li>
-          <p><a href="http://wp.dinamiko.com/demos/dkpdf/doc/dkpdf-remove-shortcode/" target="_blank">See more info here</a></p>
-      </ul>
+        <h1>Welcome to DK PDF <?php echo DKPDF_VERSION; ?></h1>
+        <h2 style="font-size:140%;">What's new in this version:</h2>
+        <ul>
+            <li>
+                <h3 style="margin-top:20px;">DK PDF admin menu</h3>
+                <?php
+                $img1 = plugins_url('assets/images/dkpdf-admin-menu.jpg', DKPDF_PLUGIN_FILE);
+                ?>
+                <img style="margin-bottom:20px;width:100%;height:auto;" src="<?php echo $img1; ?>">
+            </li>
+            <li>
+                <h3 style="margin-top:20px;">PDF Setup tab for adjusting page orientation, font size
+                    and margins of the PDF</h3>
+                <?php
+                $img2 = plugins_url('assets/images/dkpdf-setup-tab.jpg', DKPDF_PLUGIN_FILE);
+                ?>
+                <img style="margin-bottom:20px;width:100%;height:auto;" src="<?php echo $img2; ?>">
+            </li>
+            <li>
+                <h3 style="margin-top:20px;">[dkpdf-remove] shortcode for removing pieces of content
+                    in the generated PDF</h3></li>
+            <p><a href="http://wp.dinamiko.com/demos/dkpdf/doc/dkpdf-remove-shortcode/"
+                  target="_blank">See more info here</a></p>
+        </ul>
 
     </div>
 
@@ -43,28 +51,30 @@ function dkpdf_welcome_page(){ ?>
 add_action('admin_menu', 'dkpdf_welcome_screen_page');
 
 /**
-* Fires when plugin is activated or upgraded
-*/
-function dkpdf_welcome_redirect( $plugin ) {
+ * Fires when plugin is activated or upgraded
+ */
+function dkpdf_welcome_redirect($plugin)
+{
 
-   if( $plugin == 'dk-pdf/dk-pdf.php' ) {
+    if ($plugin == 'dk-pdf/dk-pdf.php') {
 
-       wp_redirect( admin_url( 'index.php?page=dkpdf-welcome' ) );
-       die();
+        wp_redirect(admin_url('index.php?page=dkpdf-welcome'));
+        die();
 
-   }
+    }
 }
 
-add_action( 'activated_plugin', 'dkpdf_welcome_redirect' );
+add_action('activated_plugin', 'dkpdf_welcome_redirect');
 
 /**
-* removes dkpdf-welcome link in Dashboard submenu 
-*/
-function dkpdf_remove_menu_entry(){
-    remove_submenu_page( 'index.php', 'dkpdf-welcome' );
+ * removes dkpdf-welcome link in Dashboard submenu
+ */
+function dkpdf_remove_menu_entry()
+{
+    remove_submenu_page('index.php', 'dkpdf-welcome');
 }
 
-add_action( 'admin_head', 'dkpdf_remove_menu_entry' );
+add_action('admin_head', 'dkpdf_remove_menu_entry');
 
 
 
