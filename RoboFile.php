79df6eca1c1bf8d29d8573f8109d127dfe2deec0
dinamiko <e.castells@inpsyde.com>
2018-10-20 11:19:36 +0200

    Add robo build task
    #68

---
+++ RoboFile.php
@@ -0,0 +1,59 @@
+<?php
+
+class RoboFile extends \Robo\Tasks
+{
+
+    public function test()
+    {
+//        $this->taskFilesystemStack()
+//            ->mkdir('logs')
+//            ->touch('logs/stuff.txt')
+//            ->run();
+//
+//        $this->taskDeleteDir('logs')->run();
+        $this->taskFilesystemStack()
+            ->mkdir('tmp-build')
+            ->copy('composer.json', 'tmp-build/composer.json')
+            ->run();
+
+        $this->_exec('composer install -d ./tmp-build --no-dev -o');
+    }
+
+    public function build()
+    {
+        // create a temporary directory and copy composer.json
+        $result = $this->taskFilesystemStack()
+            ->mkdir('tmp-build')
+            ->copy('composer.json', 'tmp-build/composer.json')
+            ->run();
+
+        if (!$result->wasSuccessful()) {
+            return $result;
+        }
+
+        // run composer install in the temporary directory
+        $result = $this->_exec('composer install -d ./tmp-build --no-dev -o');
+        if (!$result->wasSuccessful()) {
+            return $result;
+        }
+
+        // create dk-pdf.zip
+        $result = $this->taskPack("dk-pdf.zip")
+            ->addDir('dk-pdf/vendor', 'tmp-build/vendor')
+            ->addDir('dk-pdf/assets', 'assets')
+            ->addDir('dk-pdf/languages', 'languages')
+            ->addDir('dk-pdf/src', 'src')
+            ->addDir('dk-pdf/templates', 'templates')
+            ->addFile('dk-pdf/dk-pdf.php', 'dk-pdf.php')
+            ->addFile('dk-pdf/readme.txt', 'readme.txt')
+            ->run();
+
+        if (!$result->wasSuccessful()) {
+            return $result;
+        }
+
+        // delete temporary folder
+        $result = $this->taskDeleteDir('tmp-build')->run();
+        return $result;
+    }
+}
